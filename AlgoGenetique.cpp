a5430e630b7ce41c5fd14196ac4163680772bed8
PhO <hotom95@gmail.com>
2018-01-26 11:56:05 +0100

    test

---
+++ AlgoGenetique.cpp
@@ -0,0 +1,18 @@
+/*
+ * AlgoGenetique.cpp
+ *
+ *  Created on: 26 janv. 2018
+ *      Author: emacedegastines
+ */
+
+#include "AlgoGenetique.h"
+
+AlgoGenetique::AlgoGenetique() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AlgoGenetique::~AlgoGenetique() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ AlgoGenetique.h
@@ -0,0 +1,17 @@
+/*
+ * AlgoGenetique.h
+ *
+ *  Created on: 26 janv. 2018
+ *      Author: emacedegastines
+ */
+
+#ifndef ALGOGENETIQUE_H_
+#define ALGOGENETIQUE_H_
+
+class AlgoGenetique {
+public:
+	AlgoGenetique();
+	virtual ~AlgoGenetique();
+};
+
+#endif /* ALGOGENETIQUE_H_ */
